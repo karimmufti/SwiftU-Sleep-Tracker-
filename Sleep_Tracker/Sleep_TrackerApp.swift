7428cdb3a12bcc544cb7776c79d9d1889ed998ca
karimmufti <karroomdj2005@gmail.com>
2025-07-17 16:46:27 -0500

    Initial Commit

---
+++ Sleep_Tracker/Sleep_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_TrackerApp.swift
+//  Sleep_Tracker
+//
+//  Created by Karim Mufti on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
